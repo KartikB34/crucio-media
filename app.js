d8c6e7fac334c70f1bc67a601da56651326a411b
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-12-11 01:47:48 +0530

    now?

--- app.js
+++ app.js
@@ -24,11 +24,17 @@ app.use("/api/v1",user)
 
 
 //doing at last
+// app.use(express.static(path.join(__dirname, "./frontend/build")));
+
+// app.get("*", (req,res) => {
+//     res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
+// })
+
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
-})
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname +"/frontend/build/index.html"));
+});
 
 
 module.exports = app;
