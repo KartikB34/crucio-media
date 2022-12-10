684bb8da81cec6d59e6a425fb9d4fa20a07d3c80
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-12-11 01:41:47 +0530

    removed error

--- app.js
+++ app.js
@@ -27,11 +27,7 @@ app.use("/api/v1",user)
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "./frontend/build/index.html")),
-
-    function (err) {
-        res.status(500).send(err)
-    }
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
 })
 
 
