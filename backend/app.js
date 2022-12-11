2743ed232a6a57dd0a673b4c8019574280e4661f
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-12-11 10:50:31 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use("/api/v1",user)
 // app.get("*", (req,res) => {
 //     res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
 // })
-
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", function (req, res) {
